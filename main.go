3cc4d467d5b4a5ba4d26bc8abf352052c869ffdd
dogukanqlub <dogukan.atalay@qlub.io>
2025-02-08 23:58:42 +0300

    module

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
-import (
-	"log"
-
-	"github.com/dogukanatalay1/message-app-go/db/postgres" // Importing from GitHub
-)
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	// Initialize the database connection
-	postgres.ConnectDB()
-	defer postgres.CloseDB() // Ensure the database connection is closed when the app stops
-
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	app.Listen(":3000")
 }
